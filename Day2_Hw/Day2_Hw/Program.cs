742ae4114459b66d8650f04f822dc4519c5a0b88
Mashhurakhon <mashhurakhon03@gmail.com>
2025-03-06 12:41:58 +0500

    day2_homework

---
+++ Day2_Hw/Day2_Hw/Program.cs
@@ -0,0 +1,41 @@
+﻿enum operations
+{
+    plus,
+    minus,
+    multiply,
+    division
+}
+
+class Program
+{
+    static void Main()
+    {
+        double calc(double a, double b, operations operation)
+        {
+            double res;
+            switch (operation)
+            {
+                case operations.plus:
+                    res = a + b;
+                    break;
+                case operations.minus:
+                    res = a - b;
+                    break;
+                case operations.multiply:
+                    res = a * b;
+                    break;
+                case operations.division:
+                    res = a / b;
+                    break;
+                default:
+                    res = 0;
+                    break;
+            }
+
+            return res;
+        }
+
+        double calculation = calc(10, 15, operations.multiply);
+        Console.WriteLine($"Результат вычисления: {calculation}");
+    }
+}
